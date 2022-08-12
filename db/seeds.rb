32073f1a75340f8087f24de042ef5dbd48176cb8
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-08-12 11:34:00 -0500

    all spells from db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,20 @@
-# Add seed data in this file
+puts "Seeding spells..."
+# these are the spells we want to add to the database
+response = RestClient.get "https://www.dnd5eapi.co/api/spells"
+spells = JSON.parse(response)
+
+
+# iterate over each spell
+spells["results"].each do |spell|
+  spell_response = RestClient.get "https://www.dnd5eapi.co#{spell["url"]}"
+  new_spell = JSON.parse(spell_response)
+
+
+  Spell.create(
+    name: new_spell["name"],
+    level: new_spell["level"],
+    description: new_spell["desc"][0]
+  )
+end
+
+puts "Done seeding!"
